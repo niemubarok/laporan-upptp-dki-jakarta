8a9f7f06fe5daa3f9ea320a4eb36e61788446ed8
niemubarok <niemubarok@gmail.com>
2021-04-15 13:47:22 +0700

    change home component

--- dist/spa/js/7.1c8d62ce.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[7],{"142a":function(n,e,t){"use strict";t.r(e);var r=t("7a23");function a(n,e,t,a,c,u){return Object(r["B"])(),Object(r["h"])("h1",null,"tes")}var c={};c.render=a;e["default"]=c}}]);
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,10 @@
 <template>
   <q-page>
-    <q-card class="q-pa-xs q-ma-md">
-
-      <!-- <template-laporan /> -->
-      <!-- <tes /> -->
-    </q-card>
+    <!-- <q-card class="q-pa-xs q-ma-md"> -->
+    <surats />
+    <!-- <template-laporan /> -->
+    <!-- <tes /> -->
+    <!-- </q-card> -->
   </q-page>
 </template>
 
@@ -12,9 +12,10 @@
 import TemplateLaporan from 'src/components/TemplateLaporan.vue';
 import Tes from 'src/components/tes.vue';
 import { defineComponent } from 'vue';
+import Surats from '../components/Surats'
 
 export default defineComponent({
-  components: { TemplateLaporan, Tes },
+  components: { Surats },
   name: 'PageIndex'
 })
 </script>
