a372a4a5b2a7464d8d6303c73ff413a986f4117a
niemubarok <niemubarok@gmail.com>
2021-04-21 15:28:02 +0700

    fix step1

---
+++ src/components/step1.vue
@@ -0,0 +1,192 @@
+<template>
+  <div
+    class="row"
+    style="margin:auto"
+  >
+    <div
+      style="width:30%"
+      class="q-gutter-sm q-mr-md"
+      :class="$q.screen.lt.sm ? 'full-width':''"
+    >
+
+      <!-- no surat -->
+      <q-input
+        clearable
+        standout="bg-blue-10 text-yellow-14"
+        v-model="surat.noSurat"
+        label="Nomor Surat"
+        class="q-mt-sm "
+        autofocus
+      >
+        <template v-slot:prepend>
+          <q-icon name="tag" />
+        </template>
+      </q-input>
+
+      <!-- tanggal Surat -->
+      <q-input
+        standout="bg-blue-10 text-yellow-14"
+        v-model="surat.tglSurat"
+        :shadow-text="surat.tglSurat == '' ? '< Klik ikon kalender untuk pilih tanggal' : '' "
+        label-slot
+        clearable
+        class="q-mt-sm cursor-pointer"
+      >
+        <template v-slot:label>
+          Tanggal Surat / Pemeriksaan Pohon
+        </template>
+
+        <template v-slot:prepend>
+          <q-icon
+            name="event"
+            class="cursor-pointer"
+          >
+            <q-popup-proxy
+              ref="qDateProxy"
+              transition-show="scale"
+              transition-hide="scale"
+            >
+              <q-date
+                v-model="surat.tglSurat"
+                :locale="state.localDate"
+                mask="DD MMMM YYYY"
+              >
+                <div class="row items-center justify-end">
+                  <q-btn
+                    v-close-popup
+                    label="Close"
+                    color="primary"
+                    flat
+                  />
+                </div>
+              </q-date>
+            </q-popup-proxy>
+          </q-icon>
+        </template>
+
+      </q-input>
+
+      <!-- kelurahan -->
+      <q-input
+        clearable
+        standout="bg-blue-10 text-yellow-14"
+        v-model="surat.kelurahan"
+        label="Kelurahan PMPTSP"
+        class="q-mt-sm q-ml-sm"
+        :class="$q.screen.lt.sm ? 'q-mb-sm':''"
+      >
+        <template v-slot:prepend>
+          <q-icon name="home_work" />
+        </template>
+      </q-input>
+    </div>
+
+    <div
+      style="width:30%"
+      class="q-gutter-sm q-mr-lg"
+      :class="$q.screen.lt.sm ? 'full-width':''"
+    >
+      <!-- no spipp -->
+      <q-input
+        clearable
+        standout="
+            bg-blue-10
+            text-yellow-14"
+        v-model="surat.noSPIPP"
+        label="No. Surat  Permohonan Izin Penebangan Pohon"
+        class="q-mt-sm"
+      >
+        <template v-slot:prepend>
+          <q-icon name="tag" />
+        </template>
+      </q-input>
+
+      <!-- Tanggal Terima -->
+      <q-input
+        standout="bg-blue-10 text-yellow-14"
+        v-model="surat.tanggalTerimaSurat"
+        :shadow-text="surat.tanggalTerimaSurat == '' ? '< Klik ikon kalender untuk pilih tanggal' : '' "
+        label-slot
+        clearable
+        class="q-mt-sm"
+      >
+        <template v-slot:label>
+          Tanggal Terima Surat
+        </template>
+
+        <template v-slot:prepend>
+          <q-icon
+            name="event"
+            class="cursor-pointer"
+          >
+            <q-popup-proxy
+              ref="qDateProxy"
+              transition-show="scale"
+              transition-hide="scale"
+            >
+              <q-date
+                v-model="surat.tanggalTerimaSurat"
+                :locale="state.localDate"
+                mask="DD MMMM YYYY"
+              >
+                <div class="row items-center justify-end">
+                  <q-btn
+                    v-close-popup
+                    label="Close"
+                    color="primary"
+                    flat
+                  />
+                </div>
+              </q-date>
+            </q-popup-proxy>
+          </q-icon>
+        </template>
+
+      </q-input>
+
+      <!-- //nama ka unit -->
+      <q-input
+        clearable
+        standout="bg-blue-10 text-yellow-14"
+        v-model="surat.namaKaUnit"
+        label="Nama Ka Unit"
+        class="q-mt-sm q-mb-md"
+      >
+        <template v-slot:prepend>
+          <q-icon name="account_box" />
+        </template>
+      </q-input>
+    </div>
+    <div
+      style="width:30%"
+      class="q-gutter-sm"
+      :class="$q.screen.lt.sm ? 'full-width q-ml-md':''"
+    >
+
+      <!-- lokasiPohon -->
+      <q-input
+        clearable
+        filled
+        v-model="surat.alamatPohon"
+        label="Alamat Pemohon / Lokasi Pohon"
+        class="q-mt-sm"
+        type="textarea"
+      >
+        <template v-slot:prepend>
+          <q-icon name="place" />
+        </template>
+      </q-input>
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: ['surat', 'state']
+}
+</script>
+
+<style>
+</style>
--- src/pages/CreateSurat.vue
+++ src/pages/CreateSurat.vue
@@ -35,190 +35,10 @@
             icon="assignment"
             :done="state.step > 1"
           >
-            <!-- caption="Lengkapi detail surat berikut" -->
-
-            <div
-              class="row"
-              style="margin:auto"
-            >
-              <!-- style="margin-top:-20px;" -->
-              <div
-                style="width:30%"
-                class="q-gutter-sm q-mr-md"
-                :class="$q.screen.lt.sm ? 'full-width':''"
-              >
-
-                <!-- no surat -->
-                <q-input
-                  clearable
-                  standout="bg-blue-10 text-yellow-14"
-                  v-model="surat.noSurat"
-                  label="Nomor Surat"
-                  class="q-mt-sm "
-                  autofocus
-                >
-                  <template v-slot:prepend>
-                    <q-icon name="tag" />
-                  </template>
-                </q-input>
-
-                <!-- tanggal Surat -->
-                <q-input
-                  standout="bg-blue-10 text-yellow-14"
-                  v-model="surat.tglSurat"
-                  :shadow-text="surat.tglSurat == '' ? '< Klik ikon kalender untuk pilih tanggal' : '' "
-                  label-slot
-                  clearable
-                  class="q-mt-sm cursor-pointer"
-                >
-                  <template v-slot:label>
-                    Tanggal Surat / Pemeriksaan Pohon
-                  </template>
-
-                  <template v-slot:prepend>
-                    <q-icon
-                      name="event"
-                      class="cursor-pointer"
-                    >
-                      <q-popup-proxy
-                        ref="qDateProxy"
-                        transition-show="scale"
-                        transition-hide="scale"
-                      >
-                        <q-date
-                          v-model="surat.tglSurat"
-                          :locale="state.localDate"
-                          mask="DD MMMM YYYY"
-                        >
-                          <div class="row items-center justify-end">
-                            <q-btn
-                              v-close-popup
-                              label="Close"
-                              color="primary"
-                              flat
-                            />
-                          </div>
-                        </q-date>
-                      </q-popup-proxy>
-                    </q-icon>
-                  </template>
-
-                </q-input>
-
-                <!-- kelurahan -->
-                <q-input
-                  clearable
-                  standout="bg-blue-10 text-yellow-14"
-                  v-model="surat.kelurahan"
-                  label="Kelurahan PMPTSP"
-                  class="q-mt-sm q-ml-sm"
-                  :class="$q.screen.lt.sm ? 'q-mb-sm':''"
-                >
-                  <template v-slot:prepend>
-                    <q-icon name="home_work" />
-                  </template>
-                </q-input>
-              </div>
-
-              <div
-                style="width:30%"
-                class="q-gutter-sm q-mr-lg"
-                :class="$q.screen.lt.sm ? 'full-width':''"
-              >
-                <!-- no spipp -->
-                <q-input
-                  clearable
-                  standout="
-            bg-blue-10
-            text-yellow-14"
-                  v-model="surat.noSPIPP"
-                  label="No. Surat  Permohonan Izin Penebangan Pohon"
-                  class="q-mt-sm"
-                >
-                  <template v-slot:prepend>
-                    <q-icon name="tag" />
-                  </template>
-                </q-input>
-
-                <!-- Tanggal Terima -->
-                <q-input
-                  standout="bg-blue-10 text-yellow-14"
-                  v-model="surat.tanggalTerimaSurat"
-                  :shadow-text="surat.tanggalTerimaSurat == '' ? '< Klik ikon kalender untuk pilih tanggal' : '' "
-                  label-slot
-                  clearable
-                  class="q-mt-sm"
-                >
-                  <template v-slot:label>
-                    Tanggal Terima Surat
-                  </template>
-
-                  <template v-slot:prepend>
-                    <q-icon
-                      name="event"
-                      class="cursor-pointer"
-                    >
-                      <q-popup-proxy
-                        ref="qDateProxy"
-                        transition-show="scale"
-                        transition-hide="scale"
-                      >
-                        <q-date
-                          v-model="surat.tanggalTerimaSurat"
-                          :locale="state.localDate"
-                          mask="DD MMMM YYYY"
-                        >
-                          <div class="row items-center justify-end">
-                            <q-btn
-                              v-close-popup
-                              label="Close"
-                              color="primary"
-                              flat
-                            />
-                          </div>
-                        </q-date>
-                      </q-popup-proxy>
-                    </q-icon>
-                  </template>
-
-                </q-input>
-
-                <!-- //nama ka unit -->
-                <q-input
-                  clearable
-                  standout="bg-blue-10 text-yellow-14"
-                  v-model="surat.namaKaUnit"
-                  label="Nama Ka Unit"
-                  class="q-mt-sm q-mb-md"
-                >
-                  <template v-slot:prepend>
-                    <q-icon name="account_box" />
-                  </template>
-                </q-input>
-              </div>
-              <div
-                style="width:30%"
-                class="q-gutter-sm"
-                :class="$q.screen.lt.sm ? 'full-width q-ml-md':''"
-              >
-
-                <!-- lokasiPohon -->
-                <q-input
-                  clearable
-                  filled
-                  v-model="surat.alamatPohon"
-                  label="Alamat Pemohon / Lokasi Pohon"
-                  class="q-mt-sm"
-                  type="textarea"
-                >
-                  <template v-slot:prepend>
-                    <q-icon name="place" />
-                  </template>
-                </q-input>
-
-              </div>
-            </div>
-
+            <step-1
+              :surat="surat"
+              :state="state"
+            />
           </q-step>
 
           <q-step
@@ -312,10 +132,11 @@
                     </q-tooltip>
                   </q-btn> -->
 
+                  <!-- @click="state.step - 1" -->
                   <q-btn
+                    @click="$refs.stepper.previous()"
                     v-if="state.step > 1"
                     color="blue-10"
-                    @click="$refs.stepper.previous()"
                     icon="navigate_before"
                     size="sm"
                     rounded
@@ -353,12 +174,14 @@ import Step2 from '../components/Step2'
 import Step3 from '../components/Step3'
 import DataSurat from '../global/DataSurat'
 import ComponentState from '../global/ComponentState'
+import Step1 from 'src/components/step1.vue'
 
 export default {
   components: {
     Step2,
     Step3,
-    DataSurat
+    DataSurat,
+    Step1
   },
   setup () {
     const { state } = ComponentState
@@ -376,13 +199,13 @@ export default {
       currentId()
     })
 
-    const nextStep = computed(() => {
+    const nextStep = () => {
       if (state.step <= 2) {
         state.step = state.step + 1
       } else if (state.step == 3) {
         submitSurat()
       }
-    })
+    }
 
     return {
       modelLaporan,
